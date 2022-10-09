74b521d19c1ed4ebcc2398bddfa659b17658c4ed
Nick Zheliba <nikolaj.jeliba@gmail.com>
2022-10-09 12:05:52 +0300

    add path

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
-import user from '../user.json';
-import data from '../data.json';
-import friends from "../friends.json";
-import transactions from "../transactions.json";
+import user from '../path/user.json';
+import data from '../path/data.json';
+import friends from "../path/friends.json";
+import transactions from "../path/transactions.json";
 import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
